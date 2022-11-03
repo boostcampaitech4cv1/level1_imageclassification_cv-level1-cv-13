331116b3351622fc36e10ebd89dc5eec7a665396
o2rabbit <gih5444@naver.com>
2022-11-03 01:23:59 +0000

    11-03

--- YS/EfficientNetV2S_test/train.py
+++ YS/EfficientNetV2S_test/train.py
@@ -215,10 +215,10 @@ def train(data_dir, model_dir, args):
             732, 817, 109]
     cnl_weights = [1 - (x/(sum(num_ins))) for x in num_ins]
     class_weights = torch.FloatTensor(cnl_weights).to(device)
-    criterion = nn.CrossEntropyLoss(weight=class_weights) 
+    # criterion = nn.CrossEntropyLoss(weight=class_weights) 
     
     # baseline code
-    # criterion = create_criterion(args.criterion)  # default: cross_entropy
+    criterion = create_criterion(args.criterion)  # default: cross_entropy
 
     opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: SGD
     optimizer = opt_module(
@@ -338,7 +338,7 @@ if __name__ == '__main__':
     parser.add_argument('--optimizer', type=str, default=config.optimizer, help='optimizer type (default: SGD)')
     parser.add_argument('--lr', type=float, default=config.lr, help='learning rate (default: 1e-3)')
     parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
-    # parser.add_argument('--criterion', type=str, default=config.criterion, help='criterion type (default: cross_entropy)')
+    parser.add_argument('--criterion', type=str, default=config.criterion, help='criterion type (default: cross_entropy)')
     parser.add_argument('--lr_decay_step', type=int, default=15, help='learning rate scheduler deacy step (default: 20)')
     parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
     parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
